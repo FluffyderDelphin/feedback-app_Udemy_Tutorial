e2d7f08ed4a87c0fa54bf3668cd00042b8f811cf
Alexander Urlich <ulrichalex12@yahoo.de>
2022-01-15 14:26:54 +0100

    added Card Components,Conditional Styles and PropTypes

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
 
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
@@ -15,3 +16,13 @@ function FeedbackList({ feedback }) {
 }
 
 export default FeedbackList;
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
